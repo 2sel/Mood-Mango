4c115166c3b737ecb5f370610d49aeee7d157f56
Jieun Han <yjyyls66@naver.com>
2023-01-23 11:04:32 +0900

    feat.Moods페이지 UI 1차 작업

--- src/shared/Routers.tsx
+++ src/shared/Routers.tsx
@@ -8,17 +8,18 @@ import Mypage from "../pages/Mypage";
 import { moodStorage } from "../components/common/MoodStorage";
 
 const Routers = () => {
-  return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Rank />} />
-          <Route path="/main" element={<Main />} />
-          <Route path="/mypage" element={<Mypage />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Layout>
+                <Routes>
+                    <Route path='/' element={<Moods />} />
+                    <Route path='/main' element={<Main />} />
+                    <Route path='/mypage' element={<Mypage />} />
+                    <Route path='/rank' element={<Rank />} />
+                </Routes>
+            </Layout>
+        </BrowserRouter>
+    );
 };
 
 export default Routers;
