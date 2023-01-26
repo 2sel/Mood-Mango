5d53a5639a8eb3d8b7ef300fbe3e90734bcc7af7
go <sukpo6010@gmail.com>
2023-01-26 09:35:38 +0900

    feat: 셔플제외 구현

--- src/components/Rank/Musicplayer.jsx
+++ src/components/Rank/Musicplayer.jsx
@@ -11,12 +11,23 @@ import {
 } from "../../redux/modules/musicplayer";
 import Icon from "../common/Icon";
 import Marquee from "react-fast-marquee";
+import { BiColorFill } from "react-icons/bi";
+import { BsLockFill } from "react-icons/bs";
 
 const Musicplayer = () => {
+  const [videodiplay, setVideodiplay] = useState(false);
+  const [repeatestate, setRepeateState] = useState(false);
+
   const [percentage, setPercentage] = useState(0);
   const [position, setPosition] = useState(0);
   const [marginLeft, setMarginLeft] = useState(0);
   const [progressBarWidth, setProgressBarWidth] = useState(0);
+
+  const [soundpercentage, setSoundPercentage] = useState(50);
+  const [soundposition, setSoundPosition] = useState(0);
+  const [soundmarginLeft, setSoundMarginLeft] = useState(0);
+  const [soundBarWidth, setSoundBarWidth] = useState(0);
+
   const [windowSize, setWindowSize] = useState({
     width: undefined,
     height: undefined,
@@ -26,12 +37,22 @@ const Musicplayer = () => {
 
   const rangeRef = useRef();
   const thumbRef = useRef();
+
+  const SoundrangeRef = useRef();
+  const SoundthumbRef = useRef();
+
   const reactplayerRef = useRef();
 
   const { playerdisplay, isPlay, musicnum } = useAppSelector(
     (state) => state.musicplayer
   );
   const { musics } = useAppSelector((state) => state.musics);
+
+  console.log(musics);
+
+  const [musicsdata, setMusicsData] = useState(musics.slice());
+
+  console.log(musicsdata);
 
   const dispatch = useAppDispatch();
 
@@ -62,6 +83,29 @@ const Musicplayer = () => {
     reactplayerRef.current.seekTo(e.target.value / 100, "fraction");
   };
 
+  const AudioContext = window.AudioContext || window.webkitAudioContext;
+
+  let audioContext = new AudioContext();
+
+  // window.onload = () => {
+  //   audioContext = new AudioContext();
+  // };
+  const gainNode = audioContext.createGain();
+
+  const SoundonChange = (e) => {
+    setSoundPercentage(e.target.value);
+
+    gainNode.gain.value = e.target.value / 50;
+
+    console.log(gainNode.gain.value);
+
+    // audioContext.resume().then(() => {
+    //   const gainNode = audioContext.createGain();
+    //   gainNode.gain.value = soundpercentage / 50;
+    // });
+    // console.log(audioContext.createGain().gain.value);
+  };
+
   const getCurrDuration = (data) => {
     const percent = (data.played * 100).toFixed(2);
     setPercentage(+percent);
@@ -74,12 +118,17 @@ const Musicplayer = () => {
       dispatch(getMusicNum(musicnum + 1));
     }
   };
+
   const skipBackward = () => {
     if (musicnum == 0) {
       dispatch(getMusicNum(musics.length - 1));
     } else {
       dispatch(getMusicNum(musicnum - 1));
     }
+  };
+
+  const shuffle = (array) => {
+    array.sort(() => Math.random() - 0.5);
   };
 
   useEffect(() => {
@@ -105,94 +154,175 @@ const Musicplayer = () => {
     setPosition(percentage);
     setMarginLeft(centerThumb);
     setProgressBarWidth(centerProgressBar);
+
     durationSet();
   }, [percentage, windowSize]);
+
+  useEffect(() => {
+    const soundrangeWidth =
+      SoundrangeRef.current?.getBoundingClientRect().width;
+    const soundthumbWidth =
+      SoundthumbRef.current?.getBoundingClientRect().width; //전체 픽셀치
+    const soundcenterThumb = (soundthumbWidth / 100) * soundpercentage * -1;
+    const centersoundBar =
+      soundthumbWidth +
+      (soundrangeWidth / 100) * soundpercentage -
+      (soundthumbWidth / 100) * soundpercentage;
+    setSoundPosition(soundpercentage);
+    setSoundMarginLeft(soundcenterThumb);
+    setSoundBarWidth(centersoundBar);
+  }, [soundpercentage]);
 
   return (
     <>
       {playerdisplay && (
-        <MusicplayerWrap>
-          <img src={musics[musicnum].thumbnail}></img>
-          <MarqueeWrap>
-            <Marquee gradientWidth={0}>
-              <MusicInfo>
-                <MusicTitle>{musics[musicnum].title}</MusicTitle>
-                <MusicChannelTitle>
-                  {musics[musicnum].channeltitle}
-                </MusicChannelTitle>
-              </MusicInfo>
-            </Marquee>
-          </MarqueeWrap>
-          <MusicControl>
-            <div onClick={skipBackward}>
-              <Icon kind="skipback" size={16} />
-            </div>
-            <div
-              onClick={() => {
-                dispatch(togglePlay(!isPlay));
-              }}
-            >
-              {isPlay ? (
-                <Icon kind="pause" size={28} />
-              ) : (
-                <Icon kind="play" size={28} />
-              )}
-            </div>
-            <div onClick={skipForward}>
-              <Icon kind="skipforward" size={16} />
-            </div>
-          </MusicControl>
-          <MusicTime>
-            {current}/{duration}
-          </MusicTime>
-          <SliderWrap>
-            <SliderCover
-              style={{
-                width: `${progressBarWidth}px`,
-              }}
-            ></SliderCover>
-            <Thumb
-              className="thumb"
-              ref={thumbRef}
-              style={{
-                left: `${position}%`,
-                marginLeft: `${marginLeft}px`,
-              }}
-            ></Thumb>
-            <MusicRange
-              type="range"
-              value={position}
-              ref={rangeRef}
-              step="0.01"
-              onChange={onChange}
-            ></MusicRange>
-          </SliderWrap>
-          <SoundWrap>
-            <Icon kind="volume" size={20} />
-            <SoundBar type="range" min="0" max="1000" />
-            <DownButton
-              onClick={() => {
-                dispatch(PlayerToggle(false));
-              }}
-            >
-              <Icon kind="playerhide" size={30} />
-            </DownButton>
-          </SoundWrap>
-          <ReactPlayerWrap>
+        <>
+          <MusicplayerWrap>
+            <img src={musics[musicnum].thumbnail}></img>
+            <MarqueeWrap>
+              <Marquee gradientWidth={0}>
+                <MusicInfo>
+                  <MusicTitle>{musics[musicnum].title}</MusicTitle>
+                  <MusicChannelTitle>
+                    {musics[musicnum].channeltitle}
+                  </MusicChannelTitle>
+                </MusicInfo>
+              </Marquee>
+            </MarqueeWrap>
+            <MusicControl>
+              <div onClick={skipBackward}>
+                <Icon kind="skipback" size={16} />
+              </div>
+              <div
+                onClick={() => {
+                  dispatch(togglePlay(!isPlay));
+                }}
+              >
+                {isPlay ? (
+                  <Icon kind="pause" size={28} />
+                ) : (
+                  <Icon kind="play" size={28} />
+                )}
+              </div>
+              <div onClick={skipForward}>
+                <Icon kind="skipforward" size={16} />
+              </div>
+            </MusicControl>
+            <MusicTime>
+              {current}/{duration}
+            </MusicTime>
+            <SliderWrap>
+              <SliderCover
+                style={{
+                  width: `${progressBarWidth}px`,
+                }}
+              ></SliderCover>
+              <Thumb
+                className="thumb"
+                ref={thumbRef}
+                style={{
+                  left: `${position}%`,
+                  marginLeft: `${marginLeft}px`,
+                }}
+              ></Thumb>
+              <MusicRange
+                type="range"
+                value={position}
+                ref={rangeRef}
+                step="0.01"
+                onChange={onChange}
+              ></MusicRange>
+            </SliderWrap>
+            <SoundWrap>
+              <Toggle
+                onClick={() => {
+                  setVideodiplay((e) => !e);
+                }}
+              >
+                <Icon
+                  kind="video"
+                  size={20}
+                  color={videodiplay ? "#ff830a" : "#ffffff"}
+                />
+              </Toggle>
+              <Shuffle
+                onClick={() => {
+                  setMusicsData((e) => {
+                    return shuffle(e);
+                  });
+                }}
+              >
+                <Icon kind="shuffle" size={20} />
+              </Shuffle>
+              <RepeateState
+                onClick={() => {
+                  setRepeateState((e) => !e);
+                }}
+              >
+                {repeatestate ? (
+                  <Icon kind="repeatone" size={20} />
+                ) : (
+                  <Icon kind="repeatall" size={20} />
+                )}
+              </RepeateState>
+              <VolumeState>
+                <Icon kind="volume" size={20} />
+              </VolumeState>
+              <SoundSliderWrap>
+                <SoundSliderCover
+                  style={{
+                    width: `${soundBarWidth}px`,
+                  }}
+                ></SoundSliderCover>
+                <SoundThumb
+                  className="thumb"
+                  ref={SoundthumbRef}
+                  style={{
+                    left: `${soundposition}%`,
+                    marginLeft: `${soundmarginLeft}px`,
+                  }}
+                ></SoundThumb>
+                <SoundMusicRange
+                  type="range"
+                  value={soundposition}
+                  ref={SoundrangeRef}
+                  min="0"
+                  max="100"
+                  step="0.01"
+                  onChange={SoundonChange}
+                ></SoundMusicRange>
+              </SoundSliderWrap>
+              <DownButton
+                onClick={() => {
+                  dispatch(PlayerToggle(false));
+                }}
+              ></DownButton>
+            </SoundWrap>
+          </MusicplayerWrap>
+          <ReactPlayerWrap videodiplay={videodiplay}>
             <ReactPlayer
+              volume={soundpercentage / 100}
               url={musics[musicnum].url}
               ref={reactplayerRef}
               onEnded={skipForward}
+              loop={repeatestate}
               onProgress={(e) => {
                 getCurrDuration(e);
                 currentSet(e);
               }}
+              onPlay={() => {
+                dispatch(togglePlay(true));
+              }}
+              onPause={() => {
+                dispatch(togglePlay(false));
+              }}
               playing={isPlay}
               width={480}
               height={320}
+              pip={true}
             ></ReactPlayer>
           </ReactPlayerWrap>
-        </MusicplayerWrap>
+        </>
       )}
     </>
   );
@@ -200,6 +330,22 @@ const Musicplayer = () => {
 
 export default Musicplayer;
 
+const Toggle = styled.div`
+  color: #ff830a;
+  margin-left: 15px;
+  cursor: pointer;
+`;
+const Shuffle = styled.div`
+  margin-left: 15px;
+  cursor: pointer;
+`;
+const RepeateState = styled.div`
+  margin-left: 15px;
+  cursor: pointer;
+`;
+const VolumeState = styled.div`
+  margin-left: 15px;
+`;
 const MarqueeWrap = styled.div`
   width: 150px;
 `;
@@ -217,6 +363,7 @@ const MusicplayerWrap = styled.div`
   flex-direction: row;
   background-color: #000000;
   align-items: center;
+  z-index: 1;
   img {
     width: 168px;
     height: 100%;
@@ -234,7 +381,7 @@ const MusicInfo = styled.div`
   justify-content: space-around;
 `;
 const MusicTime = styled.div`
-  margin: 0 20px;
+  margin: 0 20px 0 15px;
   font-size: 14px;
   width: 60px;
   height: 100%;
@@ -243,18 +390,7 @@ const MusicTime = styled.div`
   align-items: center;
   color: #9096a2;
 `;
-const MusicTimeBar = styled.input`
-  width: calc(100% - 860px);
-  background-color: #f7f0f0;
-  height: 20px;
-`;
-const SoundBar = styled.input`
-  margin-left: 10px;
-  margin-right: 20px;
-  width: 120px;
-  background-color: #f7f0f0;
-  height: 20px;
-`;
+
 const MusicControl = styled.div`
   font-size: 16px;
   width: 100px;
@@ -262,6 +398,7 @@ const MusicControl = styled.div`
   display: flex;
   justify-content: space-around;
   align-items: center;
+  cursor: pointer;
 `;
 const MusicTitle = styled.div`
   font-size: 16px;
@@ -273,11 +410,11 @@ const MusicChannelTitle = styled.div`
   display: flex;
 `;
 const SoundWrap = styled.div`
-  padding-left: 20px;
   align-items: center;
   margin-left: auto;
+  margin-right: 15px;
   display: flex;
-  width: 240px;
+  flex-direction: row-reverse;
   font-size: 14px;
   color: #9096a2;
   display: flex;
@@ -288,9 +425,9 @@ const DownButton = styled.div``;
 const SliderWrap = styled.div`
   display: flex;
   align-items: center;
-  width: calc(100% - 860px);
+  width: calc(100% - 808px);
   position: relative;
-  background-color: #ffffff;
+  background-color: #808080;
   border-radius: 10px;
   height: 3px;
   & ::before {
@@ -353,8 +490,77 @@ const Thumb = styled.div`
   user-select: none; /*  Prevent Accidentally highlighting the number while sliding the cursor  */
 `;
 
+const SoundSliderWrap = styled.div`
+  display: flex;
+  align-items: center;
+  width: 120px;
+  position: relative;
+  background-color: #808080;
+  border-radius: 10px;
+  height: 3px;
+  & ::before {
+    content: "";
+    width: 100%;
+    height: 3px;
+    display: block;
+    position: absolute;
+    border-radius: 10px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    pointer-events: none;
+  }
+`;
+
+const SoundSliderCover = styled.div`
+  background-color: #ffffff;
+  width: 0%;
+  height: 3px;
+  display: block;
+  position: absolute;
+  border-radius: 10px;
+  top: 50%;
+  transform: translateY(-50%);
+  z-index: 1;
+  user-select: none;
+  pointer-events: none;
+`;
+
+const SoundMusicRange = styled.input`
+  -webkit-appearance: none;
+  height: 30px;
+  width: 100%;
+  cursor: pointer;
+  opacity: 0;
+  margin: 0 auto;
+  &::-webkit-slider-thumb {
+    width: 14px;
+    height: 14px;
+    background: #350f2d;
+    border: 1px solid #000000;
+    border-radius: 50%;
+    cursor: pointer;
+    -webkit-appearance: none;
+  }
+`;
+
+const SoundThumb = styled.div`
+  width: 14px;
+  height: 14px;
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.753);
+  z-index: 3;
+  background: #ffffff;
+  position: absolute;
+  border-radius: 50%;
+  top: 50%;
+  transform: translate(0%, -50%);
+  pointer-events: none; /* Remove pointer events on thumb so user can click on the actual thumb beaneath it!  */
+  user-select: none; /*  Prevent Accidentally highlighting the number while sliding the cursor  */
+`;
+
 const ReactPlayerWrap = styled.div`
   position: fixed;
   right: 20px;
-  bottom: 100px;
+  bottom: ${(props) => (props.videodiplay ? "100px" : "-500px")};
+  transition: all 0.2s;
 `;
--- src/components/common/Icon.tsx
+++ src/components/common/Icon.tsx
@@ -9,6 +9,8 @@ import {
   IoChevronDown,
   IoPause,
 } from "react-icons/io5";
+import { RiRepeat2Fill, RiRepeatOneFill, RiShuffleFill } from "react-icons/ri";
+import { MdVideoLabel } from "react-icons/md";
 
 import styled, { CSSObject } from "styled-components";
 
@@ -16,69 +18,100 @@ interface IconType {
   kind: string; // 아이콘 이름
   style?: CSSObject; // 아이콘 스타일
   size?: number; // 아이콘 사이즈
+  color?: string; // 아이콘 사이즈
   handler?: React.MouseEventHandler<HTMLDivElement>;
 }
 
-const Icon = ({ kind, style, size = 25, handler }: IconType) => {
+const Icon = ({
+  kind,
+  style,
+  size = 25,
+  handler,
+  color = "#ffffff",
+}: IconType) => {
   switch (kind) {
     case "smile":
       return (
         <StyleIconWrap>
-          <AiFillSmile style={style} size={size} />
+          <AiFillSmile style={style} size={size} color={color} />
         </StyleIconWrap>
       );
     case "music":
       return (
         <StyleIconWrap>
-          <BiMusic style={style} size={size} />
+          <BiMusic style={style} size={size} color={color} />
         </StyleIconWrap>
       );
     case "cloud":
       return (
         <StyleIconWrap onClick={handler}>
-          <BiCloud style={style} size={size} />
+          <BiCloud style={style} size={size} color={color} />
         </StyleIconWrap>
       );
     case "search":
       return (
         <StyleIconWrap>
-          <BiSearch style={style} size={size} />
+          <BiSearch style={style} size={size} color={color} />
         </StyleIconWrap>
       );
     case "play":
       return (
         <StyleIconWrap>
-          <IoPlay style={style} size={size} />
+          <IoPlay style={style} size={size} color={color} />
         </StyleIconWrap>
       );
     case "skipback":
       return (
         <StyleIconWrap>
-          <IoPlaySkipBack style={style} size={size} />
+          <IoPlaySkipBack style={style} size={size} color={color} />
         </StyleIconWrap>
       );
     case "skipforward":
       return (
         <StyleIconWrap>
-          <IoPlaySkipForward style={style} size={size} />
+          <IoPlaySkipForward style={style} size={size} color={color} />
         </StyleIconWrap>
       );
     case "volume":
       return (
         <StyleIconWrap>
-          <IoVolumeHigh style={style} size={size} />
+          <IoVolumeHigh style={style} size={size} color={color} />
         </StyleIconWrap>
       );
     case "playerhide":
       return (
         <StyleIconWrap>
-          <IoChevronDown style={style} size={size} />
+          <IoChevronDown style={style} size={size} color={color} />
         </StyleIconWrap>
       );
     case "pause":
       return (
         <StyleIconWrap>
-          <IoPause style={style} size={size} />
+          <IoPause style={style} size={size} color={color} />
+        </StyleIconWrap>
+      );
+    case "repeatall":
+      return (
+        <StyleIconWrap>
+          <RiRepeat2Fill style={style} size={size} color={color} />
+        </StyleIconWrap>
+      );
+    case "repeatone":
+      return (
+        <StyleIconWrap>
+          <RiRepeatOneFill style={style} size={size} color={color} />
+        </StyleIconWrap>
+      );
+    case "shuffle":
+      return (
+        <StyleIconWrap>
+          <RiShuffleFill style={style} size={size} color={color} />
+        </StyleIconWrap>
+      );
+    case "video":
+      return (
+        <StyleIconWrap>
+          <MdVideoLabel style={style} size={size} color={color} />
         </StyleIconWrap>
       );
     default:
@@ -86,7 +119,6 @@ const Icon = ({ kind, style, size = 25, handler }: IconType) => {
   }
 };
 const StyleIconWrap = styled.div`
-  color: #fff;
   text-align: center;
 `;
 export default Icon;
