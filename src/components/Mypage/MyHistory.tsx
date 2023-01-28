7c15ab962bb221cf050c61d896199bc3f7ba17d6
freesian12 <zzte123@naver.com>
2023-01-28 11:15:35 +0900

    refactory] 마이페이지,모달 수정

--- src/components/Mypage/MyHistory.tsx
+++ src/components/Mypage/MyHistory.tsx
@@ -1,72 +1,59 @@
 import { useState } from "react";
 import { DataType, moodStorage } from "../common/MoodStorage";
 import styled from "styled-components";
-import Item from "./ImageCircle";
-import Icon from "../common/Icon";
-import { useEffect } from "react";
-import { useAppDispatch } from "../../hooks/hooks";
-import { getPlaylist } from "../../redux/modules/musics";
 
 import Paging from "../common/Paging";
 
-export const allPlay = (playList: DataType[]) => {};
 const MyHistory = () => {
   const [mangoHistory, setMangoHistory] = useState(
     moodStorage.getMangoHistory()
   );
-  const [nowPage, setNowPage] = useState(0);
+  const [indexPage, setIndexPage] = useState(0);
   // 끝 페이지 설정
-  const [totalPage, setTotalPage] = useState(
+  const [totalCount, setTotalCount] = useState(
     moodStorage.getMangoHistory().length ?? 0
   );
 
-  const itemPopper = (data: any, folderName?: any) => {
+  const itemPopper = (data: any) => {
     if (moodStorage.popMangoHistory(data.id)) {
       alert("삭제 성공");
       setMangoHistory(moodStorage.getMangoHistory());
       // 데이터 하나를 지웠으니 totalPage 값에서 -1 하기
-      setTotalPage(totalPage - 1);
+      setTotalCount(totalCount - 1);
 
-      setNowPage(() => {
+      setIndexPage(() => {
         // 한 페이지 아이템들을 다 지웠을 경우 그 전에 페이지로 이동시키게 하기
-        return nowPage === totalPage - 1 ? nowPage - 5 : nowPage;
+        return indexPage === (totalCount - 1) / 5 ? indexPage - 1 : indexPage;
       });
     }
   };
   const clearHistory = () => {
     if (moodStorage.clearMangoHistory()) {
       alert("전체 기록 삭제 하였습니다.");
-      setTotalPage(0);
-      setNowPage(0);
+      setTotalCount(0);
+      setIndexPage(0);
       setMangoHistory(moodStorage.getMangoHistory());
     }
   };
 
   return (
     <>
-      <div style={{ display: "flex" }}>
+      <Introduce>
         내가 들었던 곡
-        <p
-          onClick={clearHistory}
-          style={{
-            color: "grey",
-            fontSize: 15,
-            display: mangoHistory.length === 0 ? "none" : "block",
-          }}
-        >
+        <Clear onClick={clearHistory} display={mangoHistory.length}>
           전체삭제
-        </p>
-      </div>
-      <ThisContents style={{ textAlign: "center", display: "flex" }}>
+        </Clear>
+      </Introduce>
+      <ThisContents>
         {mangoHistory.length !== 0 ? (
           <>
             <Paging
               dataList={mangoHistory}
               setHandler={setMangoHistory}
-              nowPage={nowPage}
-              setNowPage={setNowPage}
-              totalPage={totalPage}
-              setTotalPage={setTotalPage}
+              indexPage={indexPage}
+              setIndexPage={setIndexPage}
+              totalCount={totalCount}
+              setTotalCount={setTotalCount}
               itemPopper={itemPopper}
             />
           </>
@@ -86,8 +73,21 @@ const ThisContents = styled.div`
   display: flex;
   margin: 10px 0px;
   flex-direction: row;
+  height: 300px;
 `;
 
 const Alarm = styled.p`
   font-size: 20px;
+`;
+
+const Introduce = styled.div`
+  display: flex;
+`;
+
+const Clear = styled.p<{ display: number }>`
+  color: grey;
+  font-size: 15px;
+  display: ${(props) => (!!props.display ? "flex" : "none")};
+  margin-top: 28px;
+  cursor: pointer;
 `;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import styled from "styled-components";
 import Icon from "../components/common/Icon";
 import { getMusic, resetPlaylist } from "../redux/modules/musics";
@@ -6,8 +6,8 @@ import { useAppSelector, useAppDispatch } from "../hooks/hooks";
 import BeatLoader from "react-spinners/BeatLoader";
 import MusicContainer from "../components/Rank/MusicContainer";
 import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
-
+import { useEffect } from "react";
+import Modal from "../components/common/Modal";
 const Main = () => {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
@@ -79,7 +79,13 @@ const Main = () => {
     //     return dispatch(getMusic(id));
     // }
   };
-
+  const [modalOpen, setModalOpen] = useState(false);
+  const [videoData, setVideoData] = useState({});
+  // 모달창 노출
+  const showModal = (data: any) => {
+    setModalOpen(true);
+    setVideoData(data);
+  };
   useEffect((): any => {
     return () => dispatch(resetPlaylist()); //unmount 될때 return문이 실행 되고 callback으로 dispatch 보내줌
   }, []);
